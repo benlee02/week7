1f98d31e53a10890e31587453f3bd4a423534f9f
benjaminlee <c0023583@my.shu.ac.uk>
2020-11-10 15:14:51 +0000

    i added simple code

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int tempF = 88;
+
+        System.out.println("The temperature in degrees celcius is " + fahrenToCelcius(tempF));
+
+        int tempC=34;
+
+        System.out.println("The temperature in degrees fahrenheit is " + celcToFahren(tempC));
+
+    }
+
+    public void menu(){
+
+    }
+
+
+    static double fahrenToCelcius(int temp){
+        double celcius=(temp - 32)/1.8;
+        return celcius;
+    }
+
+    static double celcToFahren(int temp){
+        double fahren= (temp*1.8) +32;
+        return fahren;
+    }
+}
